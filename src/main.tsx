87bc87f7f580ce0953d47f3bd036c0776050b40d
Matti Svensson <matti.s.58@web.de>
2024-04-16 18:12:09 +0200

    add project base path

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/algoexplorer">
     <App />
   </BrowserRouter>
 )
